3313248882e6f37c6a94456934a9b993e709f197
Akhil Singh <akhil.singh@infra.market>
2023-07-31 20:50:12 +0530

    html templating

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,88 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+const puppeteer = require('puppeteer');
+const handlebars = require('handlebars');
+const app = express();
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+app.use(cors({origin: true}));
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+app.post('/jsonToPdf', async (req, res) => {
+    const data = req.body;
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+    // Convert JSON to HTML using a Handlebars template
+    const template = `
+  <!doctype html>
+  <html>
+    <head>
+      <title>Resume of {{basics.name}}</title>
+      <style>
+        body { font-family: Arial, sans-serif; }
+        h2 { color: #333; }
+        p { margin: 0; }
+        .section { margin-bottom: 20px; }
+      </style>
+    </head>
+    <body>
+      <h1>{{basics.name}}</h1>
+      <p>{{basics.label}}</p>
+      <p>{{basics.email}}</p>
+      <p>{{basics.phone}}</p>
+      <p>{{basics.url}}</p>
+      <p>{{basics.summary}}</p>
+      <p>{{basics.location.address}}, {{basics.location.city}}, {{basics.location.region}}, {{basics.location.countryCode}}, {{basics.location.postalCode}}</p>
+      
+      <div class="section">
+        <h2>Skills</h2>
+        {{#each skills}}
+          <h3>{{this.name}}</h3>
+          <p>{{this.level}}</p>
+          <p>{{this.keywords}}</p>
+        {{/each}}
+      </div>
+
+      <div class="section">
+        <h2>Education</h2>
+        {{#each education}}
+          <h3>{{this.institution}} ({{this.startDate}} - {{this.endDate}})</h3>
+          <p>{{this.studyType}}, {{this.area}}</p>
+          <p>Courses: {{this.courses}}</p>
+        {{/each}}
+      </div>
+
+      <div class="section">
+        <h2>Awards</h2>
+        {{#each awards}}
+          <p>{{this.title}} - {{this.date}} - {{this.awarder}}</p>
+          <p>{{this.summary}}</p>
+        {{/each}}
+      </div>
+
+      <div class="section">
+        <h2>Certificates</h2>
+        {{#each certificates}}
+          <p>{{this.name}} - {{this.date}} - {{this.issuer}}</p>
+          <p><a href="{{this.url}}">Certificate Link</a></p>
+        {{/each}}
+      </div>
+    </body>
+  </html>`;
+
+    const html = handlebars.compile(template)(data);
+
+    // Convert HTML to PDF using Puppeteer
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.setContent(html, { waitUntil: 'networkidle0' });
+    const pdf = await page.pdf({ format: 'A4', printBackground: true });
+
+    await browser.close();
+
+    // Send PDF as a binary data
+    res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length });
+    res.send(pdf);
+});
+
+
+// Specify the region for the function
+exports.api = functions.region('asia-south1').https.onRequest(app);
