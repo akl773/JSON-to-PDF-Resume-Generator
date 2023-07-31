d4db191721a6745bb5e1327345b1055ed2fd6540
Akhil Singh <akhil.singh@infra.market>
2023-07-31 21:09:09 +0530

    add index js

--- functions/index.js
+++ functions/index.js
@@ -2,87 +2,130 @@ const functions = require('firebase-functions');
 const express = require('express');
 const cors = require('cors');
 const puppeteer = require('puppeteer');
-const handlebars = require('handlebars');
+const ejs = require('ejs');
 const app = express();
 
 app.use(cors({origin: true}));
+app.use(express.json());
 
 app.post('/jsonToPdf', async (req, res) => {
     const data = req.body;
-
-    // Convert JSON to HTML using a Handlebars template
+    const basics = data.basics
+    console.log(basics)
     const template = `
-  <!doctype html>
-  <html>
-    <head>
-      <title>Resume of {{basics.name}}</title>
-      <style>
-        body { font-family: Arial, sans-serif; }
-        h2 { color: #333; }
-        p { margin: 0; }
-        .section { margin-bottom: 20px; }
-      </style>
-    </head>
-    <body>
-      <h1>{{basics.name}}</h1>
-      <p>{{basics.label}}</p>
-      <p>{{basics.email}}</p>
-      <p>{{basics.phone}}</p>
-      <p>{{basics.url}}</p>
-      <p>{{basics.summary}}</p>
-      <p>{{basics.location.address}}, {{basics.location.city}}, {{basics.location.region}}, {{basics.location.countryCode}}, {{basics.location.postalCode}}</p>
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Resume of <%= data.basics?.name %></title>
+    <style>
+      body { 
+        font-family: Arial, sans-serif;
+        color: #444;
+        margin: 0;
+        padding: 0;
+      }
+      .container {
+        margin: 0 auto;
+        max-width: 800px;
+        padding: 30px;
+      }
+      h1, h2, h3 {
+        color: #333;
+      }
+      h1 {
+        text-align: center;
+        margin-bottom: 30px;
+      }
+      .section {
+        margin-bottom: 30px;
+      }
+      .subsection {
+        margin-bottom: 10px;
+      }
+      .skills ul, .education ul {
+        list-style-type: none;
+        padding: 0;
+      }
+      .skills li, .education li {
+        margin-bottom: 10px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="container">
+      <h1><%= data.basics?.name %></h1>
+      <p><%= data.basics?.label %></p>
+      <p>Email: <%= data.basics?.email %></p>
+      <p>Phone: <%= data.basics?.phone %></p>
+      <p>Website: <%= data.basics?.url %></p>
+      <p>Summary: <%= data.basics?.summary %></p>
+      <p>Location: <%= data.basics?.location?.address %>, <%= data.basics?.location?.city %>, <%= data.basics?.location?.region %>, <%= data.basics?.location?.countryCode %>, <%= data.basics?.location?.postalCode %></p>
       
-      <div class="section">
+      <div class="section skills">
         <h2>Skills</h2>
-        {{#each skills}}
-          <h3>{{this.name}}</h3>
-          <p>{{this.level}}</p>
-          <p>{{this.keywords}}</p>
-        {{/each}}
+        <ul>
+        <% data.skills?.forEach(function(skill) { %>
+          <li>
+            <h3><%= skill.name %></h3>
+            <p>Level: <%= skill.level %></p>
+            <p>Keywords: <%= skill.keywords?.join(', ') %></p>
+          </li>
+        <% }); %>
+        </ul>
       </div>
 
-      <div class="section">
+      <div class="section education">
         <h2>Education</h2>
-        {{#each education}}
-          <h3>{{this.institution}} ({{this.startDate}} - {{this.endDate}})</h3>
-          <p>{{this.studyType}}, {{this.area}}</p>
-          <p>Courses: {{this.courses}}</p>
-        {{/each}}
+        <ul>
+        <% data.education?.forEach(function(edu) { %>
+          <li>
+            <h3><%= edu.institution %> (<%= edu.startDate %> - <%= edu.endDate %>)</h3>
+            <p>Study: <%= edu.studyType %>, <%= edu.area %></p>
+            <p>Courses: <%= edu.courses?.join(', ') %></p>
+          </li>
+        <% }); %>
+        </ul>
       </div>
 
-      <div class="section">
+      <div class="section awards">
         <h2>Awards</h2>
-        {{#each awards}}
-          <p>{{this.title}} - {{this.date}} - {{this.awarder}}</p>
-          <p>{{this.summary}}</p>
-        {{/each}}
+        <% data.awards?.forEach(function(award) { %>
+          <div class="subsection">
+            <p><%= award.title %> - <%= award.date %> - <%= award.awarder %></p>
+            <p><%= award.summary %></p>
+          </div>
+        <% }); %>
       </div>
 
-      <div class="section">
+      <div class="section certificates">
         <h2>Certificates</h2>
-        {{#each certificates}}
-          <p>{{this.name}} - {{this.date}} - {{this.issuer}}</p>
-          <p><a href="{{this.url}}">Certificate Link</a></p>
-        {{/each}}
+        <% data.certificates?.forEach(function(certificate) { %>
+          <div class="subsection">
+            <p><%= certificate.name %> - <%= certificate.date %> - <%= certificate.issuer %></p>
+            <p><a href="<%= certificate.url %>">Certificate Link</a></p>
+          </div>
+        <% }); %>
       </div>
-    </body>
-  </html>`;
+    </div>
+  </body>
+</html>`;
 
-    const html = handlebars.compile(template)(data);
+
+    const html = ejs.render(template, {data: data});
 
     // Convert HTML to PDF using Puppeteer
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.setContent(html, { waitUntil: 'networkidle0' });
-    const pdf = await page.pdf({ format: 'A4', printBackground: true });
+    await page.setContent(html, {waitUntil: 'networkidle0'});
+    const pdf = await page.pdf({format: 'A4', printBackground: true});
 
     await browser.close();
 
     // Send PDF as a binary data
-    res.set({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length });
+    res.set({'Content-Type': 'application/pdf', 'Content-Length': pdf.length});
     res.send(pdf);
 });
-
 
 // Specify the region for the function
 exports.api = functions.region('asia-south1').https.onRequest(app);
