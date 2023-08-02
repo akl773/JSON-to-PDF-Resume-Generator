d25af02c24fc0bee74ec19a15e7385d56725712a
Akhil Singh <akhil.singh@infra.market>
2023-08-02 13:54:51 +0530

    use ejs template file

--- functions/index.js
+++ functions/index.js
@@ -5,36 +5,24 @@ const express = require('express');
 const cors = require('cors');
 const puppeteer = require('puppeteer');
 const ejs = require('ejs');
-const template = require('./template');
 const app = express();
+const template = fs.readFileSync('resume.ejs', 'utf-8');
 
 app.use(cors({origin: true}));
 app.use(express.json());
 
 app.post('/jsonToPdf', async (req, res) => {
     const data = req.body;
-    const basics = data.basics
-    console.log(basics)
-
     const html = ejs.render(template, {data: data});
-
-    // Convert HTML to PDF using Puppeteer
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.setContent(html, {waitUntil: 'networkidle0'});
-    const pdf = await page.pdf({format: 'A4', printBackground: true});
+    const pdf = await page.pdf({format: 'A4', printBackground: true, waitFor: 1000});
 
     await browser.close();
 
-    // Save the PDF to a file on the system storage
-    const pdfFilePath = path.join(__dirname, 'output.pdf');
-
-    fs.writeFileSync(pdfFilePath, pdf);
-
-// Respond with a success message or other relevant data
-//     res.json({ message: 'PDF saved successfully!' });
-    res.send(html)
+    res.contentType('application/pdf');
+    res.send(pdf);
 });
 
-// Specify the region for the function
 exports.api = functions.region('asia-south1').https.onRequest(app);
